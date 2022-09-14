d9a5f71566572920db4d3ce71b558b53c053dada
Rohit Raj <rohitraj.shivoham@gmail.com>
2022-09-14 23:24:13 +0530

    Adding program of palindrome & primeNo

---
+++ palindrom.js
@@ -0,0 +1,17 @@
+
+
+   
+    const len = string.length;
+
+   
+    for (let i = 0; i < len / 2; i++) {
+
+        
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+console.log(value);
---
+++ primeNo.js
@@ -0,0 +1,7 @@
+ let flag = "Yes";
+    for(let i =2; i<num; i++){
+        if(num%i==0){
+            flag = "No";
+        }
+    }
+    console.log(flag);
